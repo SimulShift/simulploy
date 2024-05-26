ef2755a3fe32e654570d4739b7da30b87fbebb80
Alex Peña <arpenacoding@gmail.com>
2024-05-25 21:05:57 -0700

    trying new setup with tmp folders for certs

--- envoy/start-envoy.sh
+++ envoy/start-envoy.sh
@@ -3,5 +3,21 @@
 # Echo the current user
 echo "Running as user: $(whoami)"
 
-# Start Envoy
-envoy -c /etc/envoy/envoy.yaml
+#!/usr/bin/env sh
+set -e
+
+# Define the source and destination directories
+SRC_DIR="/etc/letsencrypt/live/backend.simulshift.com"
+DEST_DIR="/tmp/letsencrypt/live/backend.simulshift.com"
+
+# Create the destination directory
+mkdir -p "$DEST_DIR"
+
+# Copy the certificate files
+cp -r "$SRC_DIR"/* "$DEST_DIR"/
+
+# Change the ownership of the copied files
+chown -R 101:101 "$DEST_DIR"
+
+# Execute Envoy
+exec envoy -c /etc/envoy/envoy.yaml
