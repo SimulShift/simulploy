562b3c69360700c387d6df599450ebde5974ca32
Alex Peña <arpenacoding@gmail.com>
2024-05-25 22:41:16 -0700

    copying everything inside /etc/letsencrypt isntead

--- envoy/start-envoy.sh
+++ envoy/start-envoy.sh
@@ -7,8 +7,8 @@ echo "Running as user: $(whoami)"
 set -e
 
 # Define the source and destination directories
-SRC_DIR="/etc/letsencrypt/live/backend.simulshift.com"
-DEST_DIR="/tmp/letsencrypt/live/backend.simulshift.com"
+SRC_DIR="/etc/letsencrypt"
+DEST_DIR="/tmp/letsencrypt"
 
 # Print files in source directory before copying
 echo "Files in source directory ($SRC_DIR) before copying:"
@@ -17,33 +17,26 @@ ls -l "$SRC_DIR"
 # Create the destination directory
 mkdir -p "$DEST_DIR"
 
-# Copy the certificate files
-cp -r "$SRC_DIR"/* "$DEST_DIR"/
+# Print files in destination directory after creation
+echo "Files in destination directory ($DEST_DIR) after creation:"
+ls -l "$DEST_DIR"
 
-# Change the ownership of the copied files
-chown -R 101:101 "$DEST_DIR"
+# Copy the entire letsencrypt directory recursively
+cp -r "$SRC_DIR" "$DEST_DIR"
 
-# List the copied files after changing ownership
+# Print files in destination directory after copying
 echo "Files in destination directory ($DEST_DIR) after copying:"
 ls -l "$DEST_DIR"
 
-# Check if privkey.pem exists in the destination directory
-PRIVATE_KEY="$DEST_DIR/privkey.pem"
-if [ -f "$PRIVATE_KEY" ]; then
-    echo "Privkey file exists: $PRIVATE_KEY"
-    # Check permissions and ownership of the private key file
-    echo "Checking permissions and ownership of $PRIVATE_KEY:"
-    ls -l "$PRIVATE_KEY"
-    echo "Running 'stat' command on $PRIVATE_KEY:"
-    stat "$PRIVATE_KEY"
-    # Print the content of the private key file
-    echo "Content of $PRIVATE_KEY:"
-    cat "$PRIVATE_KEY"
-else
-    echo "Error: Privkey file does not exist: $PRIVATE_KEY"
-fi
+# Change the ownership of the copied directory and its contents
+chown -R 101:101 "$DEST_DIR"
+
+# List the copied directory and its contents after changing ownership
+echo "Files in destination directory ($DEST_DIR) after changing ownership:"
+ls -lR "$DEST_DIR"
 
 # print arguments
 echo "Arguments: $*"
 
+# Execute the command as envoy user
 su-exec envoy "$@"
